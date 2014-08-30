5b35004362ef5f91b42d16ed3dc4907af7a1f5ad
Lee <jessy1092@gmail.com>
2014-08-30 21:59:10 +0800

    Split the video.
    Signed-off-by: Lee <jessy1092@gmail.com>

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,11 +2,11 @@ $(document).ready(function ()
 {
     var splitTimer = 180;
     var youtubeAPIUrl = 'http://gdata.youtube.com/feeds/api/videos/';
+    var youtubeID = '';
     $('.sidebar').sidebar({overlay: true}).sidebar('toggle');
 
     $('.submit.button').on('click', function () {
         var youtubeUrl = $('.youtubeUrl').val();
-        var youtubeID = '';
 
         // console.log(youtubeUrl);
         $('.youtubeFrame').removeClass('hidden').addClass('unhidden');
@@ -27,9 +27,51 @@ $(document).ready(function ()
             url: youtubeAPIUrl + youtubeID + '?alt=json',
             dataType: 'json'
         }).done(function (youtubeData) {
-            var youtubeDuration = youtubeData.entry.media$group.yt$duration;
+            var youtubeDuration = youtubeData.entry.media$group.yt$duration.seconds;
             console.log(youtubeDuration);
+            addSector(youtubeDuration);
+            addSectorListner();
         });
+    });
 
-    })
+    var addSector = function (num) {
+        var totalSec = 0;
+        $('.editContent .field').remove();
+        while (totalSec < num) {
+            var startTime = totalSec;
+            var endTime = (totalSec + splitTimer) > num ? num : (totalSec + splitTimer);
+            var new_item = '<div class="field"><div class="ui orange ribbon label sectorLabel " sectorStartTime=' + startTime + ' sectorEndTime=' + endTime + '>' + transSec(startTime) +' ~ ' + transSec(endTime) + '</div><div class="text"><textarea></textarea></div></div>'
+            $('.editContent .ui.form.segment').append(new_item);
+            totalSec += splitTimer;
+        }
+    }
+
+    var transSec = function (num) {
+        var time = '';
+        var tmpTime;
+        if (num >= 3600) {
+            tmpTime = parseInt(num / 3600);
+            time += tmpTime < 10 ? '0' + tmpTime : tmpTime;
+            time += ':';
+            num %= 3600;
+        }
+        if (num >= 60) {
+            tmpTime = parseInt(num / 60)
+            time += tmpTime < 10 ? '0' + tmpTime : tmpTime;
+            time += ':';
+            num %= 60;
+        }
+
+        time += num < 10 ? '0' + num : num;
+        return time;
+    };
+
+    var addSectorListner = function () {
+        $('.sectorLabel').on('click', function () {
+            var startTime = $(this).attr('sectorStartTime');
+            var endTime = $(this).attr('sectorEndTime');
+            console.log(startTime + '  ' + endTime);
+            $('.youtubeFrame').attr('src', '//www.youtube.com/embed/' + youtubeID + '?start=' + startTime + '&end=' + endTime);
+        });
+    }
 });

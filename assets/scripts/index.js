d3760e7b9186fbcb442fca649af895793ddf6c52
Lee <jessy1092@gmail.com>
2014-08-30 23:04:33 +0800

    Add backend: ethercalc.
    Signed-off-by: Lee <jessy1092@gmail.com>

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,13 @@
 $(document).ready(function ()
 {
+    var paths = location.pathname.split('/') || [];
+    var ethercalc_name = location.hash.substring(2);
+    var csv_api_source = '';
     var splitTimer = 180;
     var youtubeAPIUrl = 'http://gdata.youtube.com/feeds/api/videos/';
+    var youtubeDuration = 0;
     var youtubeID = '';
+    var history_state = {};
     $('.sidebar').sidebar({overlay: true}).sidebar('toggle');
 
     $('.submit.button').on('click', function () {
@@ -21,29 +26,42 @@ $(document).ready(function ()
         }
 
         $('.youtubeFrame').attr('src', '//www.youtube.com/embed/' + youtubeID);
+        ethercalc_name = youtubeID;
+        csv_api_source = 'https://ethercalc.org/_/'+ethercalc_name+'/csv';
 
         $.ajax({
             type: 'GET',
             url: youtubeAPIUrl + youtubeID + '?alt=json',
             dataType: 'json'
         }).done(function (youtubeData) {
-            var youtubeDuration = youtubeData.entry.media$group.yt$duration.seconds;
-            console.log(youtubeDuration);
-            addSector(youtubeDuration);
+            youtubeDuration = youtubeData.entry.media$group.yt$duration.seconds;
+            // console.log(youtubeDuration);
+            // addSector(youtubeDuration, false);
+            compileEthercalc();
             addSectorListner();
+            history.pushState(history_state,'', '/#/' + ethercalc_name);
         });
     });
 
-    var addSector = function (num) {
+    var addSector = function (num, createEthercalc) {
         var totalSec = 0;
         $('.editContent .field').remove();
         while (totalSec < num) {
             var startTime = totalSec;
             var endTime = (totalSec + splitTimer) > num ? num : (totalSec + splitTimer);
-            var new_item = '<div class="field"><div class="ui orange ribbon label sectorLabel " sectorStartTime=' + startTime + ' sectorEndTime=' + endTime + '>' + transSec(startTime) +' ~ ' + transSec(endTime) + '</div><div class="text"><textarea></textarea></div></div>'
-            $('.editContent .ui.form.segment').append(new_item);
+
+            addItem(startTime, endTime, '');
+
+            if (createEthercalc) {
+                post_ethercalc(startTime, endTime, '');
+            }
             totalSec += splitTimer;
         }
+    }
+
+    var addItem = function (startTime, endTime, content) {
+        var new_item = '<div class="field"><div class="ui orange ribbon label sectorLabel " sectorStartTime=' + startTime + ' sectorEndTime=' + endTime + '>' + transSec(startTime) +' ~ ' + transSec(endTime) + '</div><div class="text"><textarea>' + content + '</textarea></div></div>'
+        $('.editContent .ui.form.segment').append(new_item);
     }
 
     var transSec = function (num) {
@@ -70,8 +88,46 @@ $(document).ready(function ()
         $('.sectorLabel').on('click', function () {
             var startTime = $(this).attr('sectorStartTime');
             var endTime = $(this).attr('sectorEndTime');
-            console.log(startTime + '  ' + endTime);
+            // console.log(startTime + '  ' + endTime);
             $('.youtubeFrame').attr('src', '//www.youtube.com/embed/' + youtubeID + '?start=' + startTime + '&end=' + endTime);
+        });
+    }
+
+    var post_ethercalc = function (startTime, endTime, content){
+        $.ajax({
+            url: "https://ethercalc.org/_/"+ethercalc_name,
+            type: 'POST',
+            contentType: 'text/csv',
+            processData: false,
+            data: startTime + ',' + endTime + ',' + content
+        });
+    };
+
+    var compileEthercalc = function () {
+        $.get(csv_api_source).pipe(CSV.parse).done(compileJson);
+    };
+
+    var compileJson = function (rows) {
+        // console.log(rows);
+        if (rows.length == 1 && rows[0].length == 1 && rows[0][0].length == 0) {
+            addSector(youtubeDuration, true);
+        }
+
+        $('.editContent .field').remove();
+
+        $.each(rows, function (rowIndex, row) {
+            var startTime = row[0];
+            var endTime = row[1];
+            var content = row[2];
+            // console.log(startTime + ' ' + endTime + ' ' + content);
+            if (startTime == '' && endTime == '') {
+                return;
+            }
+            addItem(startTime, endTime, content);
+
+            if (rowIndex == rows.length - 1) {
+                addSectorListner();
+            }
         });
     }
 });
